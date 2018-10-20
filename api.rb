8f559680ec5e0c16b282d6b34374172e8255acc1
Gilberto Vargas <tachoguitar@gmail.com>
2018-10-20 10:54:35 -0500

    Implement validation for collitions on schedules

--- api.rb
+++ api.rb
@@ -9,6 +9,7 @@ def configure_api!
   Services.load_config!
   db = Services.database
   Event.register(:db, db[:event])
+  Schedule.register(:events, Event)
   Schedule.register(:db, db[:schedule])
   Schedule.register(:assignations, db[:assignation])
 end
--- controllers/schedule.rb
+++ controllers/schedule.rb
@@ -13,11 +13,32 @@ class Schedule
            allow_overlap: false).freeze
 
   def attach_event(event)
+    validate_colission(event)
     db = self.class.resource(:assignations)
     data = self
            .class
            .etl_fields
            .merge(event_id: event.id, schedule_id: @data[:schedule_id])
     db.insert(data)
+  end
+
+  def validate_colission(event)
+    raise 'Conflict found with event' if colission?(event)
+  end
+
+  def all_events
+    events_class = self.class.resource(:events)
+    columns = events_class.resource(:db).columns
+    self.class
+        .resource(:assignations)
+        .inner_join(:event, %i[event_id])
+        .where(schedule_id: id)
+        .select { columns.map { |column| Sequel[:event][column].as(column) } }
+        .map { |event| events_class.new(event) }
+  end
+
+  def colission?(event)
+    return false if @data[:allow_overlap]
+    all_events.any? { |event_b| event.collide?(event_b) }
   end
 end
